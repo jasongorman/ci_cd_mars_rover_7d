0dffd8d62653d519b6012af66d0a9497a8e1c553
RK <regina.kardusaite@7digital.com>
2023-06-08 12:50:06 +0100

    added parametrized test

--- test/test_turn_left.py
+++ test/test_turn_left.py
@@ -1,12 +1,15 @@
 import unittest
 
+from parameterized import parameterized
+
 from src.MarsRover import MarsRover
 
 class TestTurnLeft(unittest.TestCase):
-    def test_turn_left_facing_north(self):
-        rover = MarsRover([4, 7], 'N')
+    @parameterized.expand([('N', 'W'),('E', 'N'), ('S', 'E'), ('W', 'S')])
+    def test_turn_left_facing_north(self, start, end):
+        rover = MarsRover([4, 7], start)
         rover.rotate_left('L')
-        self.assertEqual(rover.direction, 'W')
+        self.assertEqual(rover.direction, end)
 
 
 if __name__ == '__main__':
