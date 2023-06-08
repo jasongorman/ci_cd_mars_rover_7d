7c8fc2dc0c4427a8977575a4cd6411776f8e87b9
jonm <jonlmarsh@gmail.com>
2023-06-08 12:34:41 +0100

    Added other right rotation tests

--- src/MarsRover.py
+++ src/MarsRover.py
@@ -3,6 +3,12 @@ class MarsRover:
         self.coordinates = coordinates
         self.direction = direction
 
-    def rotate_right(self, rotate):
-
+    def rotate_right(self):
+        if self.direction == 'N':
             self.direction = 'E'
+        elif self.direction == 'E':
+            self.direction = 'S'
+        elif self.direction == 'S':
+            self.direction = 'W'
+        elif self.direction == 'W':
+            self.direction = 'N'
