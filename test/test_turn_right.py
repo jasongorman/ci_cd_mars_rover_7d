c1840e4bfeaff2054d456543073d19b67c01f571
jonm <jonlmarsh@gmail.com>
2023-06-08 12:21:36 +0100

    Changed class to MarsRover

--- test/test_turn_right.py
+++ test/test_turn_right.py
@@ -1,24 +1,18 @@
 import unittest
 
 
-class Rover:
+class MarsRover:
     def __init__(self, coordinates, direction):
         self.coordinates = coordinates
         self.direction = direction
 
     def rotate_right(self, rotate):
-        if self.direction == 'N':
+
             self.direction = 'E'
-        elif self.direction == 'E':
-            self.direction = 'S'
-        elif self.direction == 'S':
-            self.direction = 'W'
-        elif self.direction == 'W':
-            self.direction = 'N'
 
 class rover(unittest.TestCase):
     def test_turn_right_facing_north(self):
-        rover = Rover([4, 7], 'N')
+        rover = MarsRover([4, 7], 'N')
         rover.rotate_right('R')
         self.assertEqual(rover.direction, 'E')
 
